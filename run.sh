934df426c9acf3eeac8681793dbaa42648757cba
yangwenmai <yangwen.yw@gmail.com>
2020-01-21 16:47:50 +0800

    chore: update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@ date_time=`date +%Y%m%d%H%M%S`
 echo $date_time
 ./Scraper
 
-git config core.sshCommand 'ssh -i ~/.ssh/id_rsa_yangwenmai'
-
 git add .
 git commit -m "docs: update `date`"
 git push origin master
