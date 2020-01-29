4d7ad62a73ba7de533efba9ee75b0789c5aaf97a
yangwenmai <yangwen.yw@gmail.com>
2020-01-29 15:55:34 +0800

    chore(runsh): update date time script

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-date_time=`date +%Y%m%d%H%M%S`
+date_time=`date +%Y-%m-%d %H:%M:%S`
 echo $date_time
 ./Scraper
 
 git add .
-git commit -m "docs: update `date`"
+git commit -m "docs: update $date_time trending"
 git push origin master
 
